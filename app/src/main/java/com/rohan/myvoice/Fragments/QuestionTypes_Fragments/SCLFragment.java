73480a7e40651d119f4da54338c5b02eaebd1ce5
andy-coding5 <andy.coding852@gmail.com>
2019-03-20 13:31:37 +0530

    rank order fragment complete

--- app/src/main/java/com/rohan/myvoice/Fragments/QuestionTypes_Fragments/SCLFragment.java
+++ app/src/main/java/com/rohan/myvoice/Fragments/QuestionTypes_Fragments/SCLFragment.java
@@ -21,6 +21,8 @@ import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
@@ -42,10 +44,12 @@ public class SCLFragment extends Fragment {
     private String q_id, q_text, response_text = "";
     private ProgressDialog progressDialog;
 
-    private TextView textView;
+    private TextView que, current, total;
     private FrameLayout frameLayout;
     private ImageView imageView;
     private WebView webView;
+    private LinearLayout linearLayout;
+
 
     private AppCompatSeekBar seekbar;
 
@@ -53,8 +57,15 @@ public class SCLFragment extends Fragment {
     String api_key;
     private SharedPreferences pref;
     private SharedPreferences.Editor editor;
+    private double min_double, max_double, step_double;
+    private int min_int, max_int, step_int;
+    private Number min_num, max_num, step_num;
+    private int step_count = 1;
 
     private Button submit_button;
+
+    Boolean DOUBLE_VALUE = false;
+    Boolean NEGATIVE_VALUE = false;
 
     public SCLFragment() {
         // Required empty public constructor
@@ -85,14 +96,16 @@ public class SCLFragment extends Fragment {
             }
         });
 
-        textView = v.findViewById(R.id.full_que);
+        que = v.findViewById(R.id.full_que);
         frameLayout = v.findViewById(R.id.frame_view);
         imageView = v.findViewById(R.id.image_view);
         webView = v.findViewById(R.id.web_view);
         submit_button = v.findViewById(R.id.submit_btn);
-
+        linearLayout = v.findViewById(R.id.linear_layout);
         seekbar = v.findViewById(R.id.appCompatSeekBar);
 
+        current = v.findViewById(R.id.current_num);
+        total = v.findViewById(R.id.total_num);
 
         //option_list_view = v.findViewById(R.id.options_list);
 
@@ -128,7 +141,7 @@ public class SCLFragment extends Fragment {
 
         api = RetroClient.getApiService();
 
-        textView.setText(q_text);       //q_text
+        que.setText(q_text);          //q_text
 
         Call<QuestionDetail> call = api.getSCLJson(api_key, "Token " + pref.getString("token", null), q_id);
 
@@ -150,7 +163,7 @@ public class SCLFragment extends Fragment {
                         } else if (!"".equals(response.body().getData().getQuestionVideoMedia())) {
                             frameLayout.setVisibility(View.VISIBLE);
                             webView.setVisibility(View.VISIBLE);
-                            // Enable Javascript
+                            //Enable Javascript
                             WebSettings webSettings = webView.getSettings();
                             webSettings.setJavaScriptEnabled(true);
                             webView.setWebViewClient(new WebViewClient());      //to load content inside the webview rather then open it in browser
@@ -165,27 +178,74 @@ public class SCLFragment extends Fragment {
                             webView.loadUrl(response.body().getData().getQuestionAudioMedia());
                         }
                     } else {
-                        //remove top constraint of seekbar from frame_view bottom to full_que bottom
-
+                        //remove top constraint of liner layout of (3 out of 10) from frame_view bottom to full_que bottom
                         ConstraintLayout constraintLayout = v.findViewById(R.id.constraint_layout);
                         ConstraintSet constraintSet = new ConstraintSet();
 
                         constraintSet.clone(constraintLayout);
 
-                        constraintSet.connect(seekbar.getId(), constraintSet.TOP, textView.getId(), constraintSet.BOTTOM, 24);
+                        constraintSet.connect(linearLayout.getId(), constraintSet.TOP, que.getId(), constraintSet.BOTTOM, 64);
 
                         constraintSet.applyTo(constraintLayout);
 
                     }
+                    current.setText(response.body().getData().getQuestionOptions().getMin());
+                    total.setText(response.body().getData().getQuestionOptions().getMax());
+
+                    //if max, min and step value we receive from response contain .(i.e its double type of value) OR contain -(i.e negative value then I use logic of DOUBLE_VALUE true)
+                    if (response.body().getData().getQuestionOptions().getMax().contains(".") ||
+                            response.body().getData().getQuestionOptions().getMin().contains(".") ||
+                            response.body().getData().getQuestionOptions().getStep().contains(".")) {
+                        DOUBLE_VALUE = true;
+                    }
+
+                    if (response.body().getData().getQuestionOptions().getMax().contains("-") ||
+                            response.body().getData().getQuestionOptions().getMin().contains("-") ||
+                            response.body().getData().getQuestionOptions().getStep().contains("-")) {
+                        NEGATIVE_VALUE = true;
+                    }
+                    //seekbar.incrementProgressBy();
 
 
+                    //if min value is int and all the values are not float (i.e all are int) then...
+                    if (!response.body().getData().getQuestionOptions().getMax().contains(".") &&
+                            !response.body().getData().getQuestionOptions().getMin().contains(".") &&
+                            !response.body().getData().getQuestionOptions().getStep().contains(".") &&
+                            "0".equals(response.body().getData().getQuestionOptions().getMin())) {
+                        DOUBLE_VALUE = false;
+                        NEGATIVE_VALUE = false;
 
-                    //Log.d("otn_response", "Max Value: " + MAX_VALUE + " MIn value: " + MIN_VALUE);
-                    //question is now load comopletely and user can now type or speak for enter his response in the edittext of response.
+                    }
 
+                    //if DOUBLE
+                    if (DOUBLE_VALUE) {
+
+                        max_double = Double.valueOf(response.body().getData().getQuestionOptions().getMax());
+                        min_double = Double.valueOf(response.body().getData().getQuestionOptions().getMin());
+                        step_double = Double.valueOf(response.body().getData().getQuestionOptions().getStep());
+
+                        double temp = min_double;
+                        String temp_string;
+                        while (temp <= max_double) {
+                            step_count++;
+                            temp = temp + step_double;
+                            temp_string = String.format("%.2f", temp);
+                            temp = Float.valueOf(temp_string);
+                        }
+                        step_count -= 2;      //because we have started from 0
+                        Log.v("seekbar", "step: " + String.valueOf(step_count));
+                        seekbar.setMax(step_count);
+
+                    } else {
+                        //it's int
+                        max_int = Integer.valueOf(response.body().getData().getQuestionOptions().getMax());
+                        min_int = Integer.valueOf(response.body().getData().getQuestionOptions().getMin());
+                        step_int = Integer.valueOf(response.body().getData().getQuestionOptions().getStep());
+                        seekbar.setMax(max_int);
+                        seekbar.incrementProgressBy(step_int);
+                        seekbar.setProgress(min_int);
+                    }
                 }
-
-
             }
 
             @Override
@@ -194,5 +254,34 @@ public class SCLFragment extends Fragment {
             }
         });
 
+
+        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (DOUBLE_VALUE) {
+                    double current_progress = (progress * step_double) + min_double;
+
+                    String current_string = String.format("%.2f", current_progress);
+
+                    Log.v("seekbar", "current (in double) " + current_string);
+
+                    current.setText(current_string);
+                } else {
+                    //int logic
+                    current.setText(String.valueOf(progress));
+                    Log.v("seekbar", "current (in int) " + current.getText());
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/rohan/myvoice/GlobalValues/PublicClass.java
+++ app/src/main/java/com/rohan/myvoice/GlobalValues/PublicClass.java
@@ -5,7 +5,7 @@ import android.provider.Settings;
 
 public class PublicClass extends Application {
     public static String FCM_TOKEN = null;
-    public static String IP = "http://192.168.1.5:8000";
+    public static String IP = "http://192.168.0.110:8000";
 
     public static String q_id = null, c_logo = null, q_title = null;        //for passing data from one fragment to another
 
